c038b01b9fafa2c571762ac1016859729de8911a
Alex Hornby <alex@hornby.org.uk>
2023-07-22 17:50:39 +0100

    use a temp BUCKLE_CACHE in tests
    this stops tests taking the happy path where the user alrady has buck2 downloaded
    
    tested with: cargo test

--- tests/integration_buck2.rs
+++ tests/integration_buck2.rs
@@ -4,7 +4,9 @@ use assert_cmd::Command;
 /// Integration test that buckle can download buck2 and run it with same arguments.
 #[test]
 fn test_buck2_latest() {
+    let tmpdir = tempfile::TempDir::new().unwrap();
     let mut cmd = Command::cargo_bin("buckle").unwrap();
+    cmd.env("BUCKLE_CACHE", tmpdir.path().as_os_str());
     cmd.arg("--version");
     let assert = cmd.assert();
     let stdout = String::from_utf8(assert.get_output().stdout.to_vec()).unwrap();
@@ -16,7 +18,9 @@ fn test_buck2_latest() {
 /// version
 #[test]
 fn test_buck2_specific_version() {
+    let tmpdir = tempfile::TempDir::new().unwrap();
     let mut cmd = Command::cargo_bin("buckle").unwrap();
+    cmd.env("BUCKLE_CACHE", tmpdir.path().as_os_str());
     cmd.env("USE_BUCK2_VERSION", "2023-07-15");
     cmd.arg("--version");
     // TODO verify the right version is download after buck2 properly states it's version
@@ -28,7 +32,9 @@ fn test_buck2_specific_version() {
 
 #[test]
 fn test_buck2_fail() {
+    let tmpdir = tempfile::TempDir::new().unwrap();
     let mut cmd = Command::cargo_bin("buckle").unwrap();
+    cmd.env("BUCKLE_CACHE", tmpdir.path().as_os_str());
     cmd.arg("--totally-unknown-argument");
     let assert = cmd.assert();
     let stderr = String::from_utf8(assert.get_output().stderr.to_vec()).unwrap();
